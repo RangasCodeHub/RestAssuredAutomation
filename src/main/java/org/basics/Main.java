970b8b8e09292a36bf6d90c85584a9138601ec38
RangasCodeHub <saisha5500@gmail.com>
2025-09-13 22:30:04 +0530

    Payloads.java file added

--- src/main/java/org/basics/Main.java
+++ src/main/java/org/basics/Main.java
@@ -1,7 +1,9 @@
 package org.basics;
 
+import files.Payloads;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,24 +11,9 @@ public class Main {
        RestAssured.baseURI="https://rahulshettyacademy.com";
        given().queryParam("key","qaclick123")
                .header("Content-Type","application/json")
-               .body("{\n" +
-                       "\"location\": {\n" +
-                       "\"lat\": -36.383494,\n" +
-                       "\"lng\": 32.427362\n" +
-                       "},\n" +
-                       "\"accuracy\": 50,\n" +
-                       "\n" +
-                       "\"name\": \"Amigo Real Estate\",\n" +
-                       "\"phone_number\": \"(+91) 983 893 3937\",\n" +
-                       "\"address\": \"29, BTY layout, cohen 09\",\n" +
-                       "\"types\": [\n" +
-                       "\"shoe park\",\n" +
-                       "\"shop\"\n" +
-                       "],\n" +
-                       "\"website\": \"http://google.com\",\n" +
-                       "\"language\": \"French-IN\"\n" +
-                       "}")
+               .body(Payloads.addPlace())
                .when().post("/maps/api/place/add/json")
-               .then().log().all().assertThat().statusCode(200);
+               .then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP"))
+               .header("Server","Apache/2.4.52 (Ubuntu)");
     }
 }
